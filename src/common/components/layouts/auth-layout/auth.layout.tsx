588a7ba28828e98dd404a3c9059f5c594c6857b4
Enestacy <muzotcka@mail.ru>
2025-02-12 18:20:49 +0300

    chore: adjust layout components

--- src/common/components/layouts/auth-layout/auth.layout.tsx
+++ src/common/components/layouts/auth-layout/auth.layout.tsx
@@ -1,66 +1,7 @@
-import { Box, Avatar, HStack, Image, Text, Button } from '@chakra-ui/react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Box, HStack, Image, Text } from '@chakra-ui/react';
+import { Outlet } from 'react-router-dom';
 import { COLORS } from '../../../constants';
-import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from '../../ui/menu';
-import { Icons } from '../../../assets/icons';
-import { Routing } from '../../../routes';
-
-const UserProfile = () => {
-  const navigate = useNavigate();
-  return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-      gap={'10px'}
-    >
-      <HStack>
-        <Avatar.Root colorPalette={'yellow'}>
-          <Avatar.Fallback name="John Doe" />
-        </Avatar.Root>
-
-        <Text
-          marginLeft="3"
-          color="white"
-          title=" jfewjbfjhbhj bjdhebjvhfdjbej jjdb 345cdbcjhbdhjsbc"
-          style={{
-            overflow: 'hidden',
-            whiteSpace: 'nowrap',
-            textOverflow: 'ellipsis',
-            maxWidth: '10vw',
-          }}
-        >
-          jfewjbfjhbhj bjdhebjvhfdjbej jjdb 345cdbcjhbdhjsbc
-        </Text>
-      </HStack>
-
-      <MenuRoot variant={'solid'}>
-        <MenuTrigger asChild>
-          <Button variant="ghost" size="sm">
-            <Icons.ChevronDown />
-          </Button>
-        </MenuTrigger>
-        <MenuContent width={200}>
-          <MenuItem value="profile">Profile</MenuItem>
-          <MenuItem
-            value="terms"
-            onClick={() => window.open('src/common/assets/terms/terms.pdf')}
-          >
-            Terms & Conditions
-          </MenuItem>
-          <MenuItem
-            value="logout"
-            color="fg.error"
-            _hover={{ bg: 'bg.error', color: 'fg.error' }}
-            onClick={() => navigate(Routing.signIn.route())}
-          >
-            Log out
-          </MenuItem>
-        </MenuContent>
-      </MenuRoot>
-    </Box>
-  );
-};
+import { UserProfile } from './components/user-profile';
 
 export const AuthLayout = () => {
   return (
---
+++ src/common/components/layouts/auth-layout/components/user-profile.styled.ts
@@ -0,0 +1,10 @@
+import { Text } from '@chakra-ui/react';
+import styled from 'styled-components';
+
+export const UserName = styled(Text)`
+  margin-left: 3;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  max-width: 10vw;
+`;

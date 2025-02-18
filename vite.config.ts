fbca5fd93a52198ab1417c2f4986643e6f17449a
Enestacy <103126316+Enestacy@users.noreply.github.com>
2025-02-18 17:34:55 +0300

    [GS-22] feat: setup app port & envs
    https://evmagia.atlassian.net/browse/GS-22

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    commonjsOptions: { transformMixedEsModules: true },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react(), tsconfigPaths()],
+    build: {
+      commonjsOptions: { transformMixedEsModules: true },
+    },
+    server: {
+      port: Number(env.VITE_APP_PORT) || 5173,
+    },
+  };
 });
